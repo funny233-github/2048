afe89bf3e1a5f4c7819aac4311765e25d6a1d9fb
funny_233 <83470506+funny233-github@users.noreply.github.com>
2024-02-11 00:26:42 +0800

    main changes: code style

--- untils.c
+++ untils.c
@@ -1,8 +1,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define GAMERAWS 4
-#define GAMECOLS 4
+#include "untils.h"
+
 int grid[GAMECOLS*GAMERAWS] = {0};
 
 int stack[4] = {0};
@@ -12,22 +12,17 @@ int score = 0;
 
 bool slid = false;
 
-typedef enum direction{
-    UP,
-    DOWN,
-    LEFT,
-    RIGHT,
-}direction;
+bool 
+is_full(){
 
-bool is_full(){
-    for (int i = 0; i < 16; i++){
-        if (grid[i] == 0)
-            return false;
-    }
+    for (int i = 0; i < 16; i++)
+        if (grid[i] == 0) return false;
+
     return true;
 }
 
-void number_generator(){
+void 
+number_generator(){
     if (is_full())
         return;
 
@@ -40,9 +35,9 @@ void number_generator(){
     }
 }
 
-void push_and_merge_element(int value){
-    if (value == 0)
-        return;
+void 
+push_and_merge_element(int value){
+    if (value == 0) return;
 
     if (top == -1 || stack[top] != value || mergedlayer >= top){
         stack[++top] = value;
@@ -54,32 +49,36 @@ void push_and_merge_element(int value){
     mergedlayer = top;
 }
 
-void reset_stack(){
+void 
+reset_stack(){
     top = -1;
     mergedlayer = -1;
     for (int i = 0;i < 4; stack[i++] = 0);
 }
 
-void print_stack(){
+void 
+print_stack(){
     printf("\n");
     for (int i = 0;i < 4; printf("%d--",stack[i++]));
     printf("\nmergedlayer:%d\ntop:%d\n\n",mergedlayer,top);
 }
 
-int* get_element(direction direc,int i,int j){
+int* 
+get_element(direction direc,int i,int j){
     switch (direc) {
-        case UP:
-            return &grid[j*GAMERAWS+i]; 
-        case DOWN:
-            return &grid[(3-j)*GAMERAWS+i];
-        case LEFT:
-            return &grid[i*GAMERAWS+j];
-        case RIGHT:
-            return &grid[i*GAMERAWS+(3-j)];
-        }
+    case UP:
+        return &grid[j*GAMERAWS+i]; 
+    case DOWN:
+        return &grid[(3-j)*GAMERAWS+i];
+    case LEFT:
+        return &grid[i*GAMERAWS+j];
+    case RIGHT:
+        return &grid[i*GAMERAWS+(3-j)];
+    }
 }
 
-void slide(direction direc){
+void 
+slide(direction direc){
     for (int i = 0;i < 4;i++){
         int before[4];
         for (int j = 0;j < 4;j++)
