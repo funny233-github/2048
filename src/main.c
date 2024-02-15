10b4a746513cc8f88347ee31c39dd60aae154302
funny_233 <83470506+funny233-github@users.noreply.github.com>
2024-02-15 22:20:07 +0800

    Subject:add version print implemented with cmake

--- src/main.c
+++ src/main.c
@@ -1,10 +1,20 @@
 #include "display.h"
+#include "mainConfigure.h"
 #include "untils.h"
 #include <ncurses.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+    // print version info
+    if (argc == 2 && strncmp(argv[1], "-v", sizeof("-v")) == 0) {
+        printf("version:%d.%d", Project2048_VERSION_MAJOR,
+               Project2048_VERSION_MINOR);
+        return 0;
+    }
+
     srand(time(NULL));
     start_curses();
     if (has_colors() == FALSE) {
